38af3b64ba311431e73aac848bce100dfad2b804
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2024-01-12 18:01:23 +0100

    added index table

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -4,11 +4,49 @@ dd($projects)
 @endphp --}}
 @section('content')
     <section class="container">
-        <h1>Projects index</h1>
-        @foreach ($projects as $project)
-            <a href="{{route('admin.projects.show', $project->id)}}">
-                <div>{{$project->name}}</div>
-            </a>
-        @endforeach
+        <div class="container mt-4">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Project Name</th>
+                        <th scope="col">Language</th>
+                        <th scope="col">Commits</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Updated</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <th>
+                                <a href="{{ route('admin.projects.show', $project->id) }}">
+                                    {{ $project->name }}
+                                </a>
+                            </th>
+                            <td>{{ $project->language }}</td>
+                            <td>{{ $project->commits }}</td>
+                            <td>{{ $project->created }}</td>
+                            <td>{{ $project->updated }}</td>
+                            <td>
+                                <button type="button" class="btn btn-light border border-secondary">
+                                    <a href="{{route('admin.projects.show', $project->id)}}">
+                                        <i class="fa-solid fa-eye"></i>
+                                    </a>
+                                </button>
+                                <button type="button" class="btn btn-light border border-secondary">
+                                    <a href="{{route('admin.projects.edit', $project->id)}}">
+                                        <i class="fa-solid fa-pen"></i>
+                                    </a>
+                                </button>
+                                <button type="button" class="btn btn-light border border-secondary">
+                                    <i class="fa-solid fa-trash"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </section>
 @endsection
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <section class="container">
-        <h1>{{$project->name}}</h1>
-        <p>section content</p>
+        <h1 class="text-center mt-3 mb-5">{{$project->name}}</h1>
+        <h2 class="text-center">Questa &egrave; la pagina del progetto {{$project->name}}</h2>
     </section>
 @endsection
